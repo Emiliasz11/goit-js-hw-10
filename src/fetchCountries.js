bcaa07f48c15cd2ce558527ef278b8b287a0d19f
EmiliaSzczepaniak52 <emiliaszczepaniak52@gmail.com>
2023-04-07 16:39:41 +0200

    fetch commit

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+function fetchCountries() {
+    const countryName =
+      'https://restcountries.com/v3.1/name/' +
+      countryInput.value.trim() +
+      '?fields=name,capital,population,flags,languages';
+    return fetch(`${countryName}`).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
+  }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+export function fetchCountries(name) {
+    const url = `https://restcountries.com/v3.1/all?fields=name,flags,capital,population,languages`;
+  
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Problem-1');
+        }
+        return response.json();
+      })
+      .then(countries => {
+        return countries;
+      })
+      .catch(error => {
+        console.error('Problem-2:', error);
+      });
+  }
