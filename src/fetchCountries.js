2ed2f189997b01c11caff9f64f55d8d85d638ceb
EmiliaSzczepaniak52 <emiliaszczepaniak52@gmail.com>
2023-04-08 00:32:22 +0200

    end commit

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,17 +1,13 @@
-export function fetchCountries(name) {
-  const url = `https://restcountries.com/v3.1/all?fields=name,flags,capital,population,languages`;
-
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Problem-1');
-      }
-      return response.json();
-    })
-    .then(countries => {
-      return countries;
-    })
-    .catch(error => {
-      console.error('Problem-2:', error);
-    });
-}
+const BASE_URL = 'https://restcountries.com/v3.1';
+	const searchParams = new URLSearchParams({
+	  fields: 'name,capital,population,flags,languages',
+	});
+	
+	export function fetchCountries(name) {
+	  return fetch(`${BASE_URL}/name/${name}?${searchParams}`).then(response => {
+	    if (!response.ok) {
+	      throw new Error(response.status);
+	    }
+	    return response.json();
+	  });
+	}
